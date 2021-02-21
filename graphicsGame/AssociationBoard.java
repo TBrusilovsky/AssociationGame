954f551ba658c6485b87d75a4144be1f97544ad6
TBrusilovsky <55800776+TBrusilovsky@users.noreply.github.com>
2021-02-20 19:32:31 -0500

    command line version is mostly functional, attempting to convert to graphic version

---
+++ graphicsGame/AssociationBoard.java
@@ -0,0 +1,86 @@
+
+import java.io.*;
+import java.util.*;
+
+public class AssociationBoard
+{
+	private AssociationList list;
+	private String finalSolution;
+	private String[] midSolutions;
+	private String[][] clueWords;
+	private ArrayList<String> usedWords;
+	private int boardSize = 4;
+
+	public AssociationBoard(String startingWord) throws IOException
+	{
+		finalSolution = startingWord;
+		list = new AssociationList();
+		usedWords = new ArrayList<String>();
+		usedWords.add(startingWord);
+
+		loadMid();
+		loadClues();
+	}
+
+	public AssociationBoard() throws IOException
+	{
+		list = new AssociationList();
+		usedWords = new ArrayList<String>();
+
+		loadFinal();
+		loadMid();
+		loadClues();
+	}
+
+	private void loadFinal()
+	{
+		finalSolution = list.randomWord();
+	}
+	private void loadMid()
+	{
+		midSolutions = new String[boardSize];
+		ArrayList<String> solutionList = list.associatedWords(boardSize,finalSolution,usedWords);
+
+		for (int i = 0; i < boardSize; i++)
+		{
+			midSolutions[i] = solutionList.get(i);
+			usedWords.add(solutionList.get(i));
+		}
+	}
+	private void loadClues()
+	{
+		clueWords = new String[boardSize][boardSize];
+
+		for (int i = 0; i <boardSize;i++)
+		{
+			ArrayList<String> wordList = list.associatedWords(boardSize,midSolutions[i],usedWords);
+
+			for (int j = 0; j < boardSize; j++)
+			{
+				clueWords[i][j] = wordList.get(j);
+				usedWords.add(wordList.get(j));
+			}
+		}
+	}
+
+	public String getClue(int r, int c)
+	{
+		return clueWords[r][c];
+	}
+
+	public boolean checkMidSolution(String guess, int c)
+	{
+		if (midSolutions[c].equals(guess)) return true;
+		return false;
+	}
+
+	public boolean checkFinalSoluton(String guess)
+	{
+		if (finalSolution.equals(guess)) return true;
+		return false;
+	}
+
+
+
+
+}
---
+++ graphicsGame/GameBoard.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class GameBoard extends JFrame
+{
+	public GameBoard()
+	{
+		super(" Association Game");
+		setSize(1000,1000);
+		setVisible(true);
+	}
+}
